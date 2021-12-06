9709336e6957cb5532d493ada4ecdd821c701b09
Maryia <malakhava@yahoo.com>
2021-12-05 23:32:03 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,27 @@
+//Today's day, time right now
+
+var todaysDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+var weekDay = moment().format('dddd');
+
+$("#currentDay").text(todaysDay);
+$("#weekDay").text(weekDay);
+
+
+
+//Save input to local storage
+
+$(".saveBtn").click(function () {
+    event.preventDefault();
+    var formValue = $(this).siblings(".form-control").val();
+    var listItem = $(this).parent().data("hour");
+
+    localStorage.setItem(listItem, formValue);
+});
+
+//Get input from local storage through loop 
+var time = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
+for (var i = 0; i < time.length; i++) {
+    var dataHour = localStorage.getItem(time[i]);
+    
+    $(".form" + time[i]).val(dataHour);
+}
